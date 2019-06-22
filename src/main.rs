fb0cb96ecce9a713f67c69eefb2dac40f4a14471
Odin Dutton <odindutton@gmail.com>
2019-06-22 12:19:05 +0200

    Introduce vga_buffer

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,15 @@
 #![no_std] // Don't link the Rust standard library
 #![no_main] // Disable all Rust-level entry points
 
-use core::panic::PanicInfo;
+mod vga_buffer;
 
-static HELLO: &[u8] = b"Hello World!";
-static VGA_BUFFER: u32 = 0xb8000;
+use core::panic::PanicInfo;
 
 #[no_mangle]
 /// This function is the entry point, since the linker looks for a function
 /// named `_start` by default.
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = VGA_BUFFER as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    vga_buffer::WRITER.lock().write_str("Hello again");
 
     loop {}
 }
